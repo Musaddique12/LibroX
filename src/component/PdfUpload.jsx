98008ba30476ff9b088598dc12acde60aab886e6
Musaddique12 <musaddiquemomin018@gmail.com>
2025-07-31 18:06:02 +0530

    change method to cloudinary

--- src/component/PdfUpload.jsx
+++ src/component/PdfUpload.jsx
@@ -1,12 +1,31 @@
 import { client } from "filestack-react";
 
+// export const handleUpload = async (file) => {
+//   try {
+//     const filestackClient = client.init('A5OiZtOJ3QymnEaQOUJFFz'); // Replace with your Filestack API key
+//     // Upload the single file
+//     const result = await filestackClient.upload(file);
+//     return result.url
+//   } catch (error) {
+//     console.error('Error uploading PDF:', error);
+//   }
+// };
+
 export const handleUpload = async (file) => {
-  try {
-    const filestackClient = client.init('A5OiZtOJ3QymnEaQOUJFFz'); // Replace with your Filestack API key
-    // Upload the single file
-    const result = await filestackClient.upload(file);
-    return result.url
-  } catch (error) {
-    console.error('Error uploading PDF:', error);
-  }
-};
+const data = new FormData();
+      data.append("file", file);
+      data.append("upload_preset", "LibroX");
+
+      const res = await fetch(
+        "https://api.cloudinary.com/v1_1/dy0p6iio6/auto/upload",
+        {
+          method: "POST",
+          body: data,
+        }
+      );
+
+      const uploadedImageURL = await res.json();
+      console.log(uploadedImageURL);
+      console.log(uploadedImageURL.secure_url);
+      return uploadedImageURL.secure_url;
+}
